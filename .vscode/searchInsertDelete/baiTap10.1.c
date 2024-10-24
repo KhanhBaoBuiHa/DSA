cee6100bd52f55b8a9d2ffede53432f2fa62ba2c
Tiêuu <khanhbao0806@gmail.com>
2024-10-25 00:44:33 +0700

    Create baiTap10.1.c

---
+++ .vscode/searchInsertDelete/baiTap10.1.c
@@ -0,0 +1,51 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdbool.h"
+//hàm này để tách chuỗi str thành chuỗi con có độ dài bằng mã độc
+void tachChuoi(char* result,char str[],int start,int l){
+    for(int i=0;i<l;i++){
+        result[i]=str[start+i];
+    }
+    result[l]='\0';
+}
+//hàm này để so sánh 2 chuỗi
+bool soSanh(char temp[],char s[]){
+    for(int i=0;i<strlen(s);i++){
+        if(temp[i]!=s[i]) return 0;
+    }
+    return 1;
+}
+//hàm này để tìm kiếm mã độc
+int find(char s[],char str[]){
+    for(int i=0;i<strlen(str)-strlen(s);i++){
+        char temp[strlen(s)+1];
+        tachChuoi(temp,str,i,strlen(s));
+        if(soSanh(temp,s)) return i;
+    }
+    return -1;
+}
+int main(){
+    //khai báo và nhập
+    char str[1000];
+    int k,maDoc,cnt[100];
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    scanf("%d",&k);
+    getchar();
+    char s[k][100];
+    for(int i=0;i<k;i++){
+        fgets(s[i],sizeof(s[i]),stdin);
+        s[i][strcspn(s[i],"\n")]='\0';
+    }
+    //xuất
+    for(int i=0;i<k;i++){
+        int cnt=0;
+        int index=find(s[i],str);
+        if(index!=-1){
+            cnt++;
+            printf("%d %s %d\n",cnt,s[i],index);
+        }else{
+            printf("0 %s\n",s[i]);
+        }
+    }
+}
